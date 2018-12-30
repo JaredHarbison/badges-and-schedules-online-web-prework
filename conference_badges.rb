833c015d0252a869981fbc96d7aefb16f30b9f23
Jared Harbison <jared.harbison@gmail.com>
2018-12-30 23:20:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,8 +10,7 @@ def batch_badge_creator (attendees)
   return badges
 end
 
-def assign_rooms
-  assign_rooms (speakers)
+def assign_rooms (speakers)
   assign=[]
   speakers.each_with_index{|speaker,index|
   assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
